e295a63c05ef8b77a90301f59c62fea727b763bc
Nhan Nguyen <nnhansg@gmail.com>
2019-11-08 15:20:23 +0700

    PO-968 - Init new gem to invoke Unleashed APIs.

---
+++ lib/unleashed.rb
@@ -0,0 +1,5 @@
+require "unleashed/version"
+
+module Unleashed
+  # Your code goes here...
+end
---
+++ spec/unleashed_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Unleashed do
+  it "has a version number" do
+    expect(Unleashed::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
